4c03e6eb123b519318886df9a2396c7f6b4dfea6
capmahin <mahinulhuque1996@gmail.com>
2022-04-01 02:59:18 +0600

    install npm router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h2>T-shirt meania</h2>
+      <nav></nav>
+    </div>
+  );
+};
+
+export default Header;
