182ae9fc43b4a4fa3b93636b2466fe5f0cecc2a9
capmahin <mahinulhuque1996@gmail.com>
2022-04-03 02:50:06 +0600

    add grand pa and route it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Grandpa from "./components/Grandpa/Grandpa";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import OrderReview from "./components/OrderReview/OrderReview";
@@ -15,6 +16,7 @@ function App() {
           path="/orderreview"
           element={<OrderReview></OrderReview>}
         ></Route>
+        <Route path="/grandpa" element={<Grandpa></Grandpa>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Grandpa/Grandpa.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Grandpa.css";
+
+const Grandpa = () => {
+  return (
+    <div>
+      <h2>Grand pa</h2>
+    </div>
+  );
+};
+
+export default Grandpa;
